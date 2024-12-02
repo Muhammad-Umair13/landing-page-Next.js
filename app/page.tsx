89a7a0762bb6aa14ba1031e8c829eede120fbb22
Muhammad Umair <160301486+Muhammad-Umair13@users.noreply.github.com>
2024-12-02 18:01:13 +0500

    add files

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/website_design/header";
+import Hero_section from "../components/website_design/hero_section";
+import Footer from "../components/website_design/footer";
+
+export default function Home() {
+  return (
+   <div>
+    <Header />
+    <Hero_section/>
+    <Footer />
+   </div>
+  )
+}
